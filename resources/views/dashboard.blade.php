75b0c6d2d90fd055fae550576d3cf16ae28d6512
MORJAN <janluy.moreno@gmail.com>
2023-06-11 12:19:32 -0500

    resources/views/dashboard.blade.php => ajuste de contenedor del tamaño logo resources/views/vendor/jetstream/components/application-logo.blade.php => ajuste de el tamaño de la imagen del logo desde el componente

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-jet-application-logo />
             </div>
--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -1,5 +1,5 @@
-<div class="max-w-8xl mx-auto sm:px-4 lg:px-4">
+<div class="max-w-6xl mx-auto sm:px-4 lg:px-4">
     <div class="flex justify-center">
-        <img src="{{ asset('image/logo-pet.png?v=' . now()->format('H.s')) }}" alt="logo universal pet" style="filter: drop-shadow(5px 5px 15px #fff);width:750px">
+        <img src="{{ asset('image/logo-pet.png?v=' . now()->format('H.s')) }}" alt="logo universal pet" style="filter: drop-shadow(5px 5px 15px #fff);width:500px">
     </div>
 </div>
