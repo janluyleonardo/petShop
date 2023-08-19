09157440e80eda002b778fbf72415e68881c3eec
MORJAN <janluy.moreno@gmail.com>
2023-08-19 18:33:01 -0500

    ajustes de usuario base

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(InventorySeeder::class);
+        User::factory(1)->create();
+        // $this->call(InventorySeeder::class);
     }
 }
